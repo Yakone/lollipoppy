b44ac89631a0a851721533c35a9a7f68b967775c
Shannon <shannon.babincsak@gmail.com>
2016-01-08 22:19:46 -0600

    Add game over screen with restart option (same as win screen)

--- js/gameover.js
+++ js/gameover.js
@@ -5,11 +5,32 @@ var GameOver = function(game) {
 GameOver.prototype = {
 
   	create: function() {
+  		var gameOverText = this.game.add.text(this.game.camera.width * .5 - 150, 300, " Game Over ", {
+  			font: '65px Cheri Liney',
+  			fill: '#fff',
+  			align: 'center'
+  		});
+  		gameOverText.fixedToCamera = true;
+  		
+  		this.restartText = this.game.add.text(this.game.camera.width * .5 - 170, 400, " Press SPACE to Restart ", {
+  			font: '35px Cheri Liney',
+  			fill: '#fff',
+  			align: 'center'
+  		});
 
+  		// Might want to change this to bouncing text instead of blinking
+  		this.game.time.events.loop(Phaser.Timer.SECOND, this.blinkText, this);
+
+  		var spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  		spaceBar.onDown.addOnce(this.restartGame, this);
+	},
+
+	blinkText: function() {
+		this.restartText.visible = !this.restartText.visible;
 	},
 
 	restartGame: function() {
-		this.game.state.start("GameTitle");
+		this.game.state.start('Main');
 	}
 	
 }
